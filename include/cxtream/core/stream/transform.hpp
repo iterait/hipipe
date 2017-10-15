c5728afdb3b9a4ddec4273230467aa2bbb77d40b
Filip Matzner <floop@floop.cz>
2017-10-15 20:30:19 +0200

    Propagate types in stream::transform
    Relates to #25

--- include/cxtream/core/stream/transform.hpp
+++ include/cxtream/core/stream/transform.hpp
@@ -50,33 +50,41 @@ constexpr auto partial_transform(from_t<FromTypes...>, to_t<ToTypes...>,
 
 namespace detail {
 
-    // apply fun to each element in tuple of ranges in the given dimension
-    template<std::size_t Dim, std::size_t NOuts>
-    struct wrap_fun_for_dim
-    {
-        template<typename Fun>
-        static constexpr auto impl(Fun fun)
+    // Apply fun to each element in tuple of ranges in the given dimension.
+    template<typename Fun, std::size_t Dim, std::size_t NOuts, typename From, typename To>
+    struct wrap_fun_for_dim;
+
+    template<typename Fun, std::size_t Dim, std::size_t NOuts,
+             typename... FromTypes, typename... ToTypes>
+    struct wrap_fun_for_dim<Fun, Dim, NOuts, from_t<FromTypes...>, to_t<ToTypes...>> {
+        Fun fun;
+        using FunRef = decltype(std::cref(fun));
+
+        constexpr utility::maybe_tuple<ToTypes...>
+        operator()(std::tuple<FromTypes&...> tuple_of_ranges) const
         {
-            return [fun = std::move(fun)](auto&& tuple_of_ranges) CXTREAM_MUTABLE_LAMBDA_V {
-                assert(utility::same_size(tuple_of_ranges));
-                auto range_of_tuples =
-                  std::experimental::apply(ranges::view::zip,
-                                           std::forward<decltype(tuple_of_ranges)>(tuple_of_ranges))
-                  | ranges::view::transform(wrap_fun_for_dim<Dim-1, NOuts>::impl(fun));
-                return utility::unzip_if<(NOuts > 1)>(std::move(range_of_tuples));
-            };
+            assert(utility::same_size(tuple_of_ranges));
+            // build the function to be applied
+            wrap_fun_for_dim<FunRef, Dim-1, NOuts,
+              from_t<ranges::range_value_type_t<FromTypes>...>,
+              to_t<ranges::range_value_type_t<ToTypes>...>>
+                fun_wrapper{std::cref(fun)};
+            // transform
+            auto range_of_tuples =
+                std::experimental::apply(ranges::view::zip, std::move(tuple_of_ranges))
+              | ranges::view::transform(std::move(fun_wrapper));
+            return utility::unzip_if<(NOuts > 1)>(std::move(range_of_tuples));
         }
     };
 
-    template<std::size_t NOuts>
-    struct wrap_fun_for_dim<0, NOuts>
-    {
-        template<typename Fun>
-        static constexpr auto impl(Fun fun)
+    template<typename Fun, std::size_t NOuts, typename... FromTypes, typename... ToTypes>
+    struct wrap_fun_for_dim<Fun, 0, NOuts, from_t<FromTypes...>, to_t<ToTypes...>> {
+        Fun fun;
+
+        constexpr utility::maybe_tuple<ToTypes...>
+        operator()(std::tuple<FromTypes&...> tuple) const
         {
-            return [fun = std::move(fun)](auto&& tuple) CXTREAM_MUTABLE_LAMBDA_V {
-                return std::experimental::apply(fun, std::forward<decltype(tuple)>(tuple));
-            };
+            return std::experimental::apply(fun, std::move(tuple));
         }
     };
 
@@ -111,73 +119,46 @@ constexpr auto transform(from_t<FromColumns...> f,
                          dim_t<Dim> d = dim_t<1>{})
 {
     // wrap the function to be applied in the appropriate dimension
-    auto fun_wrapper = detail::wrap_fun_for_dim<Dim, sizeof...(ToColumns)>::impl(std::move(fun));
+    detail::wrap_fun_for_dim<
+      Fun, Dim, sizeof...(ToColumns),
+      from_t<typename FromColumns::batch_type...>,
+      to_t<typename ToColumns::batch_type...>>
+      fun_wrapper{std::move(fun)};
 
-    return stream::partial_transform(f, t, std::move(fun_wrapper),
-                                     [](auto& column) { return std::ref(column.value()); });
+    auto proj = [](auto& column) { return std::ref(column.value()); };
+    return stream::partial_transform(f, t, std::move(fun_wrapper), std::move(proj));
 }
 
 // conditional transform //
 
 namespace detail {
 
-    // This function accepts a tuple of references and returns a new tuple
-    // made by moving the values from the original tuple.
-    // If the tuple is of size 1, then it only returns the rvalue
-    // of its element (without wrapping it in a tuple).
-    // Furthermore, one can specify which elements should be taken
-    // using std::index_sequence.
-    template<std::size_t NArgs>
-    struct move_to_maybe_make_tuple
-    {
-        template<typename Tuple, std::size_t... Is>
-        static constexpr auto impl(Tuple& tuple, std::index_sequence<Is...>)
-        {
-            return std::make_tuple(std::move(std::get<Is>(tuple))...);
-        }
-    };
-
-    template<>
-    struct move_to_maybe_make_tuple<1>
-    {
-        template<typename Tuple, std::size_t I>
-        static constexpr auto impl(Tuple& tuple, std::index_sequence<I>)
-        {
-            return std::move(std::get<I>(tuple));
-        }
-    };
-
     // wrap the function to be applied only on if the first argument evaluates to true
-    template<std::size_t NOuts>
-    struct wrap_fun_with_cond
-    {
-        template<
-          typename Fun,
-          std::size_t... FromIndices,
-          std::size_t... ToIndices>
-        static constexpr auto impl(
-          Fun fun,
-          std::index_sequence<FromIndices...> from_indices,
-          std::index_sequence<ToIndices...> to_indices)
+    template<typename Fun, typename FromIdxs, typename ToIdxs, typename From, typename To>
+    struct wrap_fun_with_cond;
+
+    template<typename Fun, typename FromIdxs, std::size_t... ToIdxs,
+             typename CondCol, typename... Cols, typename... ToTypes>
+    struct wrap_fun_with_cond<Fun, FromIdxs, std::index_sequence<ToIdxs...>,
+                              from_t<CondCol, Cols...>, to_t<ToTypes...>> {
+        Fun fun;
+
+        constexpr utility::maybe_tuple<ToTypes...> operator()(CondCol& cond, Cols&... cols) const
         {
-            return [fun = std::move(fun), to_indices]
-              (auto cond, auto&... cols) CXTREAM_MUTABLE_LAMBDA_V {
-                // make a tuple of all arguments, except for the condition
-                auto args_view = std::forward_as_tuple<decltype(cols)...>(cols...);
-                // apply the function if the condition is true
-                if (cond) {
-                    // the function is applied only on a subset of the arguments
-                    // representing FromColumns
-                    return std::experimental::apply(fun,
-                      utility::tuple_index_view<FromIndices...>(args_view));
-                // return the original arguments if the condition is false
-                } else {
-                    // only a subset of the arguments representing ToColumns is returned
-                    // note: We can force std::move in here, because
-                    // we are only copying data to themselves.
-                    return move_to_maybe_make_tuple<NOuts>::impl(args_view, to_indices);
-                }
-            };
+            // make a tuple of all arguments, except for the condition
+            std::tuple<Cols&...> args_view{cols...};
+            // apply the function if the condition is true
+            if (cond) {
+                // the function is applied only on a subset of the arguments
+                // representing FromColumns
+                return std::experimental::apply(fun,
+                  utility::tuple_index_view(args_view, FromIdxs{}));
+            }
+            // return the original arguments if the condition is false
+            // only a subset of the arguments representing ToColumns is returned
+            // note: We can force std::move in here, because
+            // we are only copying data to themselves.
+            return {std::move(std::get<ToIdxs>(args_view))...};
         }
     };
 
@@ -250,17 +231,22 @@ constexpr auto transform(
     // are concatenated in a single tuple
     constexpr std::size_t n_from = sizeof...(FromColumns);
     constexpr std::size_t n_to = sizeof...(ToColumns);
-    std::make_index_sequence<n_from> from_indices;
-    utility::make_offset_index_sequence<n_from, n_to> to_indices;
+    using FromIdxs = std::make_index_sequence<n_from>;
+    using ToIdxs = utility::make_offset_index_sequence<n_from, n_to>;
 
     // wrap the function to be applied in the appropriate dimension using the condition column
-    auto prob_fun =
-      detail::wrap_fun_with_cond<n_to>::impl(std::move(fun), from_indices, to_indices);
+    detail::wrap_fun_with_cond<
+      Fun, FromIdxs, ToIdxs,
+      from_t<utility::ndim_type_t<typename CondColumn::batch_type, Dim>,
+             utility::ndim_type_t<typename FromColumns::batch_type, Dim>...,
+             utility::ndim_type_t<typename ToColumns::batch_type, Dim>...>,
+      to_t<utility::ndim_type_t<typename ToColumns::batch_type, Dim>...>>
+      cond_fun{std::move(fun)};
 
     // transform from both, FromColumns and ToColumns into ToColumns
     // the wrapper function takes care of extracting the parameters for the original function
     return stream::transform(from_t<CondColumn, FromColumns..., ToColumns...>{},
-                             t, std::move(prob_fun), d);
+                             t, std::move(cond_fun), d);
 }
 
 // probabilistic transform //
@@ -268,40 +254,39 @@ constexpr auto transform(
 namespace detail {
 
     // wrap the function to be an identity if the dice roll fails
-    template<std::size_t NOuts>
-    struct wrap_fun_with_prob
-    {
-        template<
-          typename Fun,
-          typename Prng,
-          std::size_t... FromIndices,
-          std::size_t... ToIndices>
-        static constexpr auto impl(
-          double prob,
-          Fun fun,
-          Prng& prng,
-          std::index_sequence<FromIndices...> from_indices,
-          std::index_sequence<ToIndices...> to_indices)
+    template<typename Fun, typename Prng,
+             typename FromIdxs, typename ToIdxs,
+             typename From, typename To>
+    struct wrap_fun_with_prob;
+
+    template<typename Fun, typename Prng,
+             typename FromIdxs, std::size_t... ToIdxs,
+             typename... FromTypes, typename... ToTypes>
+    struct wrap_fun_with_prob<Fun, Prng,
+                              FromIdxs, std::index_sequence<ToIdxs...>,
+                              from_t<FromTypes...>, to_t<ToTypes...>> {
+        Fun fun;
+        std::reference_wrapper<Prng> prng;
+        const double prob;
+
+        utility::maybe_tuple<ToTypes...> operator()(FromTypes&... cols) const
         {
-            return [fun = std::move(fun), &prng, prob, to_indices]
-              (auto&... cols) CXTREAM_MUTABLE_LAMBDA_V {
-                std::uniform_real_distribution<> dis(0, 1);
-                // make a tuple of all arguments
-                auto args_view = std::forward_as_tuple<decltype(cols)...>(cols...);
-                // apply the function if the dice roll succeeds
-                if (prob == 1. || (prob > 0. && dis(prng) < prob)) {
-                    // the function is applied only on a subset of the arguments
-                    // representing FromColumns
-                    return std::experimental::apply(fun,
-                      utility::tuple_index_view<FromIndices...>(args_view));
-                // return the original arguments if the dice roll fails
-                } else {
-                    // only a subset of the arguments representing ToColumns is returned
-                    // note: We can force std::move in here, because
-                    // we are only copying data to themselves.
-                    return move_to_maybe_make_tuple<NOuts>::impl(args_view, to_indices);
-                }
-            };
+            assert(prob >= 0. && prob <= 1.);
+            std::uniform_real_distribution<> dis{0, 1};
+            // make a tuple of all arguments
+            std::tuple<FromTypes&...> args_view{cols...};
+            // apply the function if the dice roll succeeds
+            if (prob == 1. || (prob > 0. && dis(prng.get()) < prob)) {
+                // the function is applied only on a subset of the arguments
+                // representing FromColumns
+                return std::experimental::apply(fun,
+                  utility::tuple_index_view(args_view, FromIdxs{}));
+            }
+            // return the original arguments if the dice roll fails
+            // only a subset of the arguments representing ToColumns is returned
+            // note: We can force std::move in here, because
+            // we are only copying data to themselves.
+            return {std::move(std::get<ToIdxs>(args_view))...};
         }
     };
 
@@ -356,12 +341,16 @@ constexpr auto transform(
     // are concatenated in a single tuple
     constexpr std::size_t n_from = sizeof...(FromColumns);
     constexpr std::size_t n_to = sizeof...(ToColumns);
-    std::make_index_sequence<n_from> from_indices;
-    utility::make_offset_index_sequence<n_from, n_to> to_indices;
+    using FromIdxs = std::make_index_sequence<n_from>;
+    using ToIdxs = utility::make_offset_index_sequence<n_from, n_to>;
 
     // wrap the function to be applied in the appropriate dimension with the given probabiliy
-    auto prob_fun = detail::wrap_fun_with_prob<n_to>::impl(
-      prob, std::move(fun), prng, from_indices, to_indices);
+    detail::wrap_fun_with_prob<
+      Fun, Prng, FromIdxs, ToIdxs,
+      from_t<utility::ndim_type_t<typename FromColumns::batch_type, Dim>...,
+             utility::ndim_type_t<typename ToColumns::batch_type, Dim>...>,
+      to_t<utility::ndim_type_t<typename ToColumns::batch_type, Dim>...>>
+      prob_fun{std::move(fun), prng, prob};
 
     // transform from both, FromColumns and ToColumns into ToColumns
     // the wrapper function takes care of extracting the parameters for the original function
