673de38514d7161a7a21086a6d9298da57ab81dc
Filip Matzner <floop@floop.cz>
2017-10-21 19:16:31 +0200

    Propagate types in cxtream::drop
    - Relates to #25.
    - Relates to #34.

--- include/cxtream/core/stream/drop.hpp
+++ include/cxtream/core/stream/drop.hpp
@@ -16,13 +16,52 @@
 
 namespace cxtream::stream {
 
-template<typename... Columns>
-constexpr auto drop_fn()
-{
-    return ranges::view::transform([](auto&& source) {
-        return utility::tuple_remove<Columns...>(std::forward<decltype(source)>(source));
-    });
-}
+namespace detail {
+
+    template<typename Source, typename... DropColumns>
+    struct drop_impl;
+
+    template<typename... SourceColumns, typename... DropColumns>
+    struct drop_impl<std::tuple<SourceColumns...>, DropColumns...> {
+
+        constexpr decltype(auto) operator()(std::tuple<SourceColumns...> source) const
+        {
+            return utility::tuple_remove<DropColumns...>(std::move(source));
+        }
+
+    };
+
+    template<typename... Columns>
+    class drop_fn {
+    private:
+        friend ranges::view::view_access;
+
+        static auto bind(drop_fn<Columns...> fun)
+        {
+            return ranges::make_pipeable(std::bind(fun, std::placeholders::_1));
+        }
+
+    public:
+        template<typename Rng, CONCEPT_REQUIRES_(ranges::ForwardRange<Rng>())>
+        constexpr auto operator()(Rng&& rng) const
+        {
+            using StreamType = ranges::range_value_type_t<Rng>;
+            return ranges::view::transform(
+              std::forward<Rng>(rng),
+              drop_impl<StreamType, Columns...>{});
+        }
+
+        /// \cond
+        template<typename Rng, CONCEPT_REQUIRES_(!ranges::ForwardRange<Rng>())>
+        void operator()(Rng&&) const
+        {
+            CONCEPT_ASSERT_MSG(ranges::ForwardRange<Rng>(),
+              "stream::drop only works on ranges satisfying the ForwardRange concept.");
+        }
+        /// \endcond
+    };
+
+}  // namespace detail
 
 /// \ingroup Stream
 /// \brief Drops columns from a stream.
@@ -35,7 +74,7 @@ constexpr auto drop_fn()
 ///     auto rng = data | create<id, value>() | drop<id>;
 /// \endcode
 template <typename... Columns>
-auto drop = drop_fn<Columns...>();
+constexpr ranges::view::view<detail::drop_fn<Columns...>> drop{};
 
 }  // end namespace cxtream::stream
 #endif
