340671d54214ba3711e88b3dba4a2d742d3fa1e0
Filip Matzner <floop@floop.cz>
2018-10-29 02:16:11 +0100

    Add stubs for batch and column tests

---
+++ test/core/stream/batch_t.cpp
@@ -0,0 +1,21 @@
+/****************************************************************************
+ *  hipipe library
+ *  Copyright (c) 2018, Iterait a.s.
+ *  Author(s) Filip Matzner
+ *
+ *  This file is distributed under the MIT License.
+ *  See the accompanying file LICENSE.txt for the complete license agreement.
+ ****************************************************************************/
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE stream_batch_t_test
+
+#include "common.hpp"
+
+#include <hipipe/core/stream/batch_t.hpp>
+
+
+BOOST_AUTO_TEST_CASE(test_insert_extract)
+{
+    BOOST_FAIL("Test not ready yet.");
+}
---
+++ test/core/stream/column_t.cpp
@@ -0,0 +1,35 @@
+/****************************************************************************
+ *  hipipe library
+ *  Copyright (c) 2018, Iterait a.s.
+ *  Author(s) Filip Matzner
+ *
+ *  This file is distributed under the MIT License.
+ *  See the accompanying file LICENSE.txt for the complete license agreement.
+ ****************************************************************************/
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE stream_column_t_test
+
+#include "common.hpp"
+
+#include <hipipe/core/stream/column_t.hpp>
+
+// TODO add some tests!
+// Don't forget:
+// - Read doc/mainpage.md and doc/installation.md.
+// - See doc/installation.md for instructions how to run one or all unit tests.
+// - Test all the public functions.
+// - Fix the docstrings if they are wrong.
+// - common.hpp contains multiple predefined columns.
+// - Look into other tests if unsure how to proceed, e.g. for_each.cpp.
+// - Test whether move-only (e.g., std::unique_ptr) values work as well.
+// - Creating move-only data can sometimes be tricky,
+//   see e.g. for_each.cpp:test_for_each_move_only.
+// - Add yourself to the list of authors.
+// - Remove this comment.
+// - Make PR targeting into branch `runtime`.
+
+BOOST_AUTO_TEST_CASE(test_insert_extract)
+{
+    BOOST_FAIL("Test not ready yet.");
+}
