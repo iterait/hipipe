79617d2b7ba8046d8fcc78921973ab052dde7e08
blazeka <blazekada@gmail.com>
2018-11-15 15:28:27 +0100

    Add keep transformer including tests

---
+++ include/hipipe/core/stream/keep.hpp
@@ -0,0 +1,70 @@
+/****************************************************************************
+ *  hipipe library
+ *  Copyright (c) 2017, Cognexa Solutions s.r.o.
+ *  Copyright (c) 2018, Iterait a.s.
+ *  Author(s) Filip Matzner, Adam Blažek
+ *
+ *  This file is distributed under the MIT License.
+ *  See the accompanying file LICENSE.txt for the complete license agreement.
+ ****************************************************************************/
+
+#pragma once
+
+#include <hipipe/core/stream/stream_t.hpp>
+
+#include <range/v3/view/transform.hpp>
+
+
+namespace hipipe::stream {
+
+namespace detail {
+
+    template<typename... Columns>
+    class keep_fn {
+    private:
+        friend ranges::view::view_access;
+
+        static auto bind(keep_fn<Columns...> fun)
+        {
+            return ranges::make_pipeable(std::bind(fun, std::placeholders::_1));
+        }
+
+    public:
+        template<typename Rng, CONCEPT_REQUIRES_(ranges::InputRange<Rng>())>
+        forward_stream_t operator()(Rng&& rng) const
+        {
+            return ranges::view::transform(std::forward<Rng>(rng),
+              [](batch_t batch) -> batch_t {
+                  batch_t result;
+                  (result.insert_or_assign<Columns>(std::move(batch.extract<Columns>())), ...);
+                  return result;
+            });
+        }
+
+        /// \cond
+        template<typename Rng, CONCEPT_REQUIRES_(!ranges::InputRange<Rng>())>
+        void operator()(Rng&&) const
+        {
+            CONCEPT_ASSERT_MSG(ranges::InputRange<Rng>(),
+              "stream::keep only works on ranges satisfying the InputRange concept.");
+        }
+        /// \endcond
+    };
+
+}  // namespace detail
+
+
+/// \ingroup Stream
+/// \brief Keep the specified columns in the stream, drop everything else.
+///
+/// Example:
+/// \code
+///     HIPIPE_DEFINE_COLUMN(id, int)
+///     HIPIPE_DEFINE_COLUMN(value, double)
+///     std::vector<std::tuple<int, double>> data = {{3, 5.}, {1, 2.}};
+///     auto rng = data | create<id, value>() | keep<value>; // now it has only the value column
+/// \endcode
+template <typename... Columns>
+ranges::view::view<detail::keep_fn<Columns...>> keep{};
+
+}  // end namespace hipipe::stream
---
+++ test/core/stream/keep.cpp
@@ -0,0 +1,129 @@
+/****************************************************************************
+ *  hipipe library
+ *  Copyright (c) 2017, Cognexa Solutions s.r.o.
+ *  Copyright (c) 2018, Iterait a.s.
+ *  Author(s) Filip Matzner, Adam Blažek
+ *
+ *  This file is distributed under the MIT License.
+ *  See the accompanying file LICENSE.txt for the complete license agreement.
+ ****************************************************************************/
+
+#define BOOST_TEST_DYN_LINK
+#define BOOST_TEST_MODULE column_keep_test
+
+#include "common.hpp"
+
+#include <hipipe/core/stream/keep.hpp>
+
+HIPIPE_DEFINE_COLUMN(Unique3, std::unique_ptr<int>)
+
+
+
+BOOST_AUTO_TEST_CASE(test_keep_int_column)
+{
+    using hipipe::stream::batch_t;
+
+    batch_t batch1, batch2;
+    std::vector<batch_t> data;
+    batch1.insert_or_assign<Int>(3);
+    batch1.insert_or_assign<Double>(5.);
+    data.push_back(std::move(batch1));
+    batch2.insert_or_assign<Int>(1);
+    batch2.insert_or_assign<Double>(2.);
+    data.push_back(std::move(batch2));
+
+    std::vector<batch_t> stream = data
+      | ranges::view::move
+      | hipipe::stream::keep<Int>;
+
+    BOOST_TEST(stream.size() == 2);
+    BOOST_TEST(stream.at(0).size() == 1);
+    BOOST_TEST(!stream.at(0).contains<Double>());
+    BOOST_TEST(stream.at(0).contains<Int>());
+    BOOST_TEST(stream.at(0).extract<Int>() == (std::vector<int>{3}));
+    BOOST_TEST(stream.at(1).size() == 1);
+    BOOST_TEST(!stream.at(1).contains<Double>());
+    BOOST_TEST(stream.at(1).contains<Int>());
+    BOOST_TEST(stream.at(1).extract<Int>() == (std::vector<int>{1}));
+
+}
+
+
+BOOST_AUTO_TEST_CASE(test_keep_unique_column)
+{
+    using hipipe::stream::batch_t;
+
+    batch_t batch1, batch2;
+    std::vector<batch_t> data;
+    batch1.insert_or_assign<Unique>();
+    batch1.extract<Unique>().push_back(std::make_unique<int>(1));
+    batch1.insert_or_assign<Unique3>();
+    batch1.extract<Unique3>().push_back(std::make_unique<int>(5));
+    data.push_back(std::move(batch1));
+    batch2.insert_or_assign<Unique>();
+    batch2.extract<Unique>().push_back(std::make_unique<int>(2));
+    batch2.insert_or_assign<Unique3>();
+    batch2.extract<Unique3>().push_back(std::make_unique<int>(6));
+    data.push_back(std::move(batch2));
+
+    std::vector<batch_t> stream = data
+      | ranges::view::move
+      | hipipe::stream::keep<Unique>;
+
+    BOOST_TEST(stream.size() == 2);
+    BOOST_TEST(stream.at(0).size() == 1);
+    BOOST_TEST(stream.at(0).contains<Unique>());
+    BOOST_TEST(!stream.at(0).contains<Unique3>());
+    BOOST_TEST(stream.at(0).extract<Unique>().size() == 1);
+    BOOST_TEST(*stream.at(0).extract<Unique>().at(0) == 1);
+    BOOST_TEST(stream.at(1).size() == 1);
+    BOOST_TEST(stream.at(1).contains<Unique>());
+    BOOST_TEST(!stream.at(1).contains<Unique3>());
+    BOOST_TEST(stream.at(1).extract<Unique>().size() == 1);
+    BOOST_TEST(*stream.at(1).extract<Unique>().at(0) == 2);
+}
+
+
+BOOST_AUTO_TEST_CASE(test_keep_multiple_columns)
+{
+    using hipipe::stream::batch_t;
+
+    batch_t batch1, batch2;
+    std::vector<batch_t> data;
+    batch1.insert_or_assign<Int>();
+    batch1.extract<Int>().push_back(7);
+    batch1.insert_or_assign<Unique>();
+    batch1.extract<Unique>().push_back(std::make_unique<int>(1));
+    batch1.insert_or_assign<Unique3>();
+    batch1.extract<Unique3>().push_back(std::make_unique<int>(5));
+    data.push_back(std::move(batch1));
+    batch2.insert_or_assign<Int>();
+    batch2.extract<Int>().push_back(4);
+    batch2.insert_or_assign<Unique>();
+    batch2.extract<Unique>().push_back(std::make_unique<int>(2));
+    batch2.insert_or_assign<Unique3>();
+    batch2.extract<Unique3>().push_back(std::make_unique<int>(6));
+    data.push_back(std::move(batch2));
+
+    std::vector<batch_t> stream = data
+      | ranges::view::move
+      | hipipe::stream::keep<Int, Unique>;
+
+    BOOST_TEST(stream.size() == 2);
+    BOOST_TEST(stream.at(0).size() == 2);
+    BOOST_TEST(stream.at(0).contains<Int>());
+    BOOST_TEST(stream.at(0).contains<Unique>());
+    BOOST_TEST(!stream.at(0).contains<Unique3>());
+    BOOST_TEST(stream.at(0).extract<Int>().size() == 1);
+    BOOST_TEST(stream.at(0).extract<Int>().at(0) == 7);
+    BOOST_TEST(stream.at(0).extract<Unique>().size() == 1);
+    BOOST_TEST(*stream.at(0).extract<Unique>().at(0) == 1);
+    BOOST_TEST(stream.at(1).size() == 2);
+    BOOST_TEST(stream.at(1).contains<Int>());
+    BOOST_TEST(stream.at(1).contains<Unique>());
+    BOOST_TEST(!stream.at(1).contains<Unique3>());
+    BOOST_TEST(stream.at(1).extract<Int>().size() == 1);
+    BOOST_TEST(stream.at(1).extract<Int>().at(0) == 4);
+    BOOST_TEST(stream.at(1).extract<Unique>().size() == 1);
+    BOOST_TEST(*stream.at(1).extract<Unique>().at(0) == 2);
+}
