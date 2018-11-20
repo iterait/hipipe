8aea0b36d1b3944ed9f1c898a903c4077c549d2c
Filip Matzner <floop@floop.cz>
2018-11-20 13:12:40 +0100

    Update docs (commit cc5b137)

--- search/functions_2.js
+++ search/functions_2.js
@@ -4,5 +4,6 @@ var searchData=
   ['cols',['cols',['../classhipipe_1_1dataframe.html#a7239f8dcab20a1575a900d04a9b09881',1,'hipipe::dataframe']]],
   ['column_5fbase',['column_base',['../classhipipe_1_1stream_1_1column__base.html#a65707e93d0711c1e09f528c6ee8d1678',1,'hipipe::stream::column_base']]],
   ['contains',['contains',['../classhipipe_1_1index__mapper.html#ab1a16464f3bfed5871d8344c7447f561',1,'hipipe::index_mapper::contains()'],['../classhipipe_1_1stream_1_1batch.html#a9ae5111808097a3fffec15cd860ef187',1,'hipipe::stream::batch::contains()']]],
+  ['copy',['copy',['../group___stream.html#ga41c2acf41cda5437ee5d73cac67266cf',1,'hipipe::stream']]],
   ['create_5ftemp_5fdirectory',['create_temp_directory',['../group___filesystem.html#gac6e2bc40b1620b4d3840bde87b71ad3b',1,'hipipe::utility']]]
 ];
